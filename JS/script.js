09ae9573996bd93c3d245aab42a4e0a6901f8eec
Bipin <bk3284488@gmail.com>
2023-03-07 11:48:01 +0530

    added favicon and modified some card CSS

--- JS/script.js
+++ JS/script.js
@@ -10,4 +10,31 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
+
+  // let Btn = document.querySelectorAll(".card-btn");
+  // Btn.addEventListener("click", showMsg);
+  //     function showMsg(e) {
+  //       if (Btn.textContent == "Add+") {
+  //         Btn.textContent = "Added";
+  //         Btn.style.backgroundColor="green";
+  //         Btn.style.color="white";
+  //       } else {
+  //         Btn.textContent = "Add+";
+  //         Btn.style.backgroundColor="aliceblue";
+  //         Btn.style.color="#0760e6";
+  //     }
+  //   }
   
+  document.querySelectorAll('.card-btn').forEach(function(e) {
+    e.addEventListener('click', function() {
+        if (this.textContent == "Add+"){
+          this.textContent = "Added"
+            this.style.backgroundColor="green"
+            this.style.color="white"
+        }else{
+          this.textContent = "Add+"
+      this.style.backgroundColor = "aliceblue";
+      this.style.color="#0760e6";
+    }
+    })
+  });
