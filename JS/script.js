e74f0adb57b6127c56e79ee83919dff64a394ff9
Bipin <bk3284488@gmail.com>
2023-03-11 08:08:42 +0530

    added search bar and back to top btn

--- JS/script.js
+++ JS/script.js
@@ -37,5 +37,14 @@ document.querySelectorAll('.card-btn').forEach(function(e) {
   })
 });
 
+// go to top js
+const toTop = document.querySelector(".go-top");
 
-
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100){
+    toTop.classList.add("active");
+  } 
+  else{
+    toTop.classList.remove("active")
+  }
+})
