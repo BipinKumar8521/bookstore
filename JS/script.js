9952f5935200fc663cd39ff525e0ae458a744fca
Bipin <bk3284488@gmail.com>
2023-03-22 08:02:37 +0530

    reduces the height of card and hides scroll

--- JS/script.js
+++ JS/script.js
@@ -47,3 +47,15 @@ window.addEventListener("scroll", () => {
     toTop.classList.remove("active");
   }
 });
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
